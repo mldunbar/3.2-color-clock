a6807a029d5d1a559e6ec5b8849efe28b09252a2
Megan Dunbar <meganleedunbar@gmail.com>
2015-05-19 18:41:08 -0400

    minor changes

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 var currentTime;
-var $timeNow = document.querySelector(".timeNow")
+var $timeNow = document.querySelector(".timeNow");
+var $hours = document.querySelector("hours");
+var $minutes = document.querySelector("minutes");
+var $seconds = document.querySelector("seconds");
+var $line = document.querySelector('.line');
+
 
 function displayTime(){
   var currentTime = new Date();
@@ -7,10 +12,37 @@ function displayTime(){
   var minutes = currentTime.getMinutes();
   var seconds = currentTime.getSeconds();
   $timeNow.textContent = hours + ":" + minutes + ":" + seconds;
+  setWidth(); //this makes it change every second
 }
-//this calls the function
+//this calls the function, so you don't need to call the function earlier
 setInterval(displayTime, 1000);
 
-function padNumbers() {
-  
+// function displayCurrentTime(time){
+//   var timeNow = padNumbers(time);
+//   $hours.textContent = time[0];
+//   $minutes.textContent = time[1];
+//   $seconds.textContent = time[2];
+// }
+//
+// function padNumbers(time){
+//   var leadingZero = [];
+//   leadingZero[0] = ("0" + time[0]);
+//   leadingZero[1] = ("0" + time[1]);
+//   leadingZero[2] = ("0" + time[2]);
+//   return leadingZero;
+// }
+
+// padNumbers(); // execute function
+
+//WAKE UP TOMORROW AND MAKE THIS WORK
+
+function setWidth() {
+  var currentTime = new Date();
+  var seconds = currentTime.getSeconds();
+  var lineWidth = (seconds/60)*100;
+  var linePercent = lineWidth + "%"; //adding a percent makes it into a string
+
+  $line.style.width = linePercent;
 }
+
+// var timeBarPercent = String(timeBar) + "%";
